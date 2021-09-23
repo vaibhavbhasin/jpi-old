7415737d3314283af260b55be9c7ea837997f121
Aditya Kumar <vaibhav.bhasin@yahoo.co.in>
2021-09-23 18:01:36 +0530

    Add User Payment list on admin
    Signed-off-by: Aditya Kumar <vaibhav.bhasin@yahoo.co.in>

---
+++ app/Models/DwollaTransactionHistory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DwollaTransactionHistory extends Model
+{
+    use HasFactory;
+
+    protected $table = 'dwolla_transaction_histories';
+    protected $fillable = [
+        'user_id',
+        'transaction_id',
+        'source_user_id',
+        'destination_user_id',
+        'funding_source_id',
+        'funding_destination_id',
+        'amount',
+        'currency',
+        'status',
+    ];
+    protected $with = ['user'];
+    public function getCreatedDateAttribute($key)
+    {
+        return Carbon::parse($this->created_at)->format(config('jpi.date_format'));
+    }
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
