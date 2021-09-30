29627e14964ced8752d59bbe1474f3218b5c63cf
Aditya Kumar <vaibhav.bhasin@yahoo.co.in>
2021-09-30 21:15:50 +0530

    add company view page
    Signed-off-by: Aditya Kumar <vaibhav.bhasin@yahoo.co.in>

---
+++ resources/views/trade_partners/admin/companies/detail.blade.php
@@ -0,0 +1,10 @@
+{{-- extend layout --}}
+@extends('layouts.contentLayoutMaster')
+{{-- page content --}}
+@section('content')
+<div id="main">
+    <div class='container'>
+
+    </div>
+</div>
+@endsection
