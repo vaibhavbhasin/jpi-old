93a494959669a94b7e50cf012a3d3b52808e5f75
Aditya Kumar <adityafullstackdeveloper@gmail.com>
2021-10-04 19:45:25 +0530

    Add svg icon
    Signed-off-by: Aditya Kumar <adityafullstackdeveloper@gmail.com>

--- resources/views/panels/submenu.blade.php
+++ resources/views/panels/submenu.blade.php
@@ -4,9 +4,9 @@
         @foreach ($sub_menus as $sub_menu)
             @if(isset($sub_menu->role))
                 @if(auth()->user()->hasRole($sub_menu->role))
-                    <li class="@if(isset($sub_menu->href_type) && $sub_menu->href_type==='route') {{ \Route::current()->getName() === $sub_menu->url ? 'active' : '' }} @else {{(request()->is($sub_menu->url.'*') ) ? 'active' : '' }} @endif">
+                    <li class="{!! is_active($sub_menu,2) !!}">
                         <a href="@if(($sub_menu->url)==='javascript:void(0)'){{$sub_menu->url}} @else @if(isset($sub_menu->href_type) && $sub_menu->href_type==='route') {{route($sub_menu->url)}} @else {{url($sub_menu->url)}} @endif @endif"
-                           class="{{@$sub_menu->class}} @if(isset($sub_menu->href_type) && $sub_menu->href_type==='route') {{ \Route::current()->getName() === $sub_menu->url ? 'active '.$configData['activeMenuColor'] : '' }} @else {{(request()->is($sub_menu->url.'*') && $sub_menu->url !=='') ? 'active '.$configData['activeMenuColor'] : '' }} @endif"
+                           class="{{@$sub_menu->class}} {{is_active($sub_menu,2)}} {{$configData['activeMenuColor']}}"
                            @if(!empty($configData['activeMenuColor'])) {{'style=background:none;box-shadow:none;'}} @endif
                            target="{{isset($sub_menu->newTab) ? '_blank':''}}">
                             <i class="material-icons">radio_button_unchecked</i>
